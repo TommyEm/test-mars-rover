af94f560a2425deffd671cd61d0cf58a0624c68c
TommyEm <tommyholyshit@gmail.com>
2021-10-03 19:03:58 +0200

    Added Mars terrain texture

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,17 @@ import { GlobalStyle } from '../src/style/GlobalStyle';
 import { themeDarkMode } from '../src/style/themes';
 
 
+// const tokenContext = require.context(
+// 	'!!raw-loader!../src/design-tokens/build/css',
+// 	true,
+// 	/.\.(css|less|scss|svg)$/
+// );
+
+// const tokenFiles = tokenContext.keys().map(function (filename) {
+// 	return { filename: filename, content: tokenContext(filename).default };
+// });
+
+
 export const parameters = {
 	actions: { argTypesRegex: "^on[A-Z].*" },
 	controls: {
@@ -11,14 +22,17 @@ export const parameters = {
 			date: /Date$/,
 		},
 	},
+	// designToken: {
+	// 	files: tokenFiles,
+	// },
 };
 
 // Wrap stories with global styles
 const withThemeProvider = (Story, context) => {
 	return (
 		<>
-			<GlobalStyle />
 			<ThemeProvider theme={themeDarkMode}>
+				<GlobalStyle />
 				<Story {...context} />
 			</ThemeProvider>
 		</>
--- src/components/grid/Grid.styled.tsx
+++ src/components/grid/Grid.styled.tsx
@@ -1,12 +1,29 @@
 import styled from 'styled-components';
 import { CELL_SIZE } from '../../constants';
+import marsBg from '../../assets/img/mars-surface.jpg';
 
 
 export const StyledGrid = styled.div<{ size: number }>`
 	display: grid;
 	grid-template: repeat(${props => props.size}, 1fr) / repeat(${props => props.size}, 1fr);
+	position: relative;
 	width: ${props => props.size * CELL_SIZE}px;
 	height: ${props => props.size * CELL_SIZE}px;
+
+	&::after {
+		content: '';
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		z-index: -1;
+		background: url(${marsBg}) center center no-repeat;
+		background-size: cover;
+		box-shadow: inset 0 0 120px #000, inset 0 0 80px #000, inset 0 0 40px #000;
+		opacity: .65;
+		transform: scale(1.25);
+	}
 
 	&,
 	& .Grid-cell {
